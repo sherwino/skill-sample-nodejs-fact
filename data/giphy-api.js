ee342e875d64465eaa538dd5739d3a51f4035510
Gustavo Gutierrez <imguss@gmail.com>
2017-12-03 09:56:14 -0500

    added rating and other fixes

--- data/giphy-api.js
+++ data/giphy-api.js
@@ -3,23 +3,34 @@
 //Data for this list came from: https://en.wikipedia.org/wiki/List_of_best-selling_books
 //=========================================================================================================================================
 
+// require request package
 const request = require('request');
 
-const baseUrl = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC";
+// giphy base url
+const baseUrl = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&rating";
 
 function findGif(query) {
 
+  // image rating
+  const rating = 'pg-13';
+
+  // request options
   const options = {
-    url: `${baseUrl}&q=${query}`,
+    url: `${baseUrl}&q=${query}&rating=${rating}`,
     dataType: 'json'
   };
 
-  request.get(options, (err, response, body) => {
+  // get request
+  request.get(options, (err, response, imageObj) => {
     if (err) {
       console.log('There was an error: ' +  err);
       return;
     }
-    return body;
+
+    // parsed images
+    const images = JSON.parse(imageObj).data;
+
+    return images;
   });
 }
 
