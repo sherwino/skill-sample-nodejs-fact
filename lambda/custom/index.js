79c097cae3ce97cb7cbfd5acc4fd833774a8ef78
Frank Hernandez <fhern077@fiu.edu>
2017-12-02 20:34:50 -0500

    ear muffs added

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -19,8 +19,8 @@ const Alexa = require('alexa-sdk');
 //Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
 const APP_ID = undefined;
 
-const SKILL_NAME = 'Space Facts';
-const GET_FACT_MESSAGE = "Here's your fact: ";
+const SKILL_NAME = 'Ear Muff';
+const GET_FACT_MESSAGE = "Here's your earmuff: ";
 const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
 const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
--- lambda/data/facts-en-US.js
+++ lambda/data/facts-en-US.js
@@ -14,8 +14,8 @@ var data = [
     "The Moon is moving approximately 3.8 cm away from our planet every year."
 ];
 
-var SKILL_NAME = "American Space Facts";
-var GET_FACT_MESSAGE = "Here's your fact: ";
+var SKILL_NAME = "Earmuffs";
+var GET_FACT_MESSAGE = "Here's your earmuff: ";
 var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
 var STOP_MESSAGE = "Goodbye!";
